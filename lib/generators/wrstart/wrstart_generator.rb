ac37a33d7833b4717f2096562d00e9f4bd16711a
Bruno dos Santos Chagas <brunoprimo@gmail.com>
2014-08-19 13:51:49 -0300

    nome da pasta template

--- lib/generators/wrstart/wrstart_generator.rb
+++ lib/generators/wrstart/wrstart_generator.rb
@@ -1,6 +1,6 @@
 class WrstartGenerator < Rails::Generators::Base
 
-  source_root File.expand_path('../templates',__FILE__)
+  source_root File.expand_path('../template',__FILE__)
   argument :app_name, :type => :string, :default => "app_name"
 
   def generate_wrstart
