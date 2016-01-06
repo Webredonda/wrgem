0e4a90ea4740e41035828d206260017d2cad6232
Bruno Chagas <brunoprimo@gmail.com>
2016-01-06 19:54:34 -0200

    atualizando gemfile

--- lib/generators/wrstart/template/application_helper.rb
+++ lib/generators/wrstart/template/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
     content_for :body_class, class_name
   end
 
+  def responsive_image(image_bg, coverable=true)
+    html = ''
+    html << "<div class='image_bg' style='background:url("+ image_url(image_bg) +") no-repeat center; width: 100%; height: 100%;"
+    html << "background-size: cover;" if coverable
+    html << "'></div>"
+
+    return raw html
+  end
+
 end
